f05c5845f0495fb78c5d51e0ff2f7d1627fd932b
Davide <doreglo@red.software.systems>
2025-04-16 11:09:41 +0200

    add welcome widget 1st solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_six/welcome.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('World!'))),
+    return MaterialApp(
+      home: Scaffold(
+        body: DefaultTextStyle.merge(
+          style: TextStyle(color: Colors.white, fontSize: 18),
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  const Color.fromARGB(255, 138, 74, 249),
+                  const Color.fromARGB(255, 100, 6, 122),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              color: Colors.white,
+            ),
+            child: Welcome(),
+          ),
+        ),
+      ),
     );
   }
 }
