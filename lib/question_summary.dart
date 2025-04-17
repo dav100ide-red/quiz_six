385b5432c692e389ae523bc4c1cf4060f3ed8e5d
Davide <doreglo@red.software.systems>
2025-04-17 12:11:44 +0200

    add SingleChildScrollView in questionSummary

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -7,25 +7,30 @@ class QuestionSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:
-          summaryData.map((sd) {
-            return Row(
-              children: [
-                Text(((sd['question_index'] as int) + 1).toString()), //
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(sd['question'] as String), //
-                      SizedBox(height: 5),
-                      Text(sd['user_answer'] as String),
-                      Text(sd['correct_answer'] as String),
-                    ],
-                  ),
-                ),
-              ],
-            );
-          }).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children:
+              summaryData.map((sd) {
+                return Row(
+                  children: [
+                    Text(((sd['question_index'] as int) + 1).toString()), //
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(sd['question'] as String), //
+                          SizedBox(height: 5),
+                          Text(sd['user_answer'] as String),
+                          Text(sd['correct_answer'] as String),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
+        ),
+      ),
     );
   }
 }
